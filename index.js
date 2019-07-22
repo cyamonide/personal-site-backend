d31b253ed55240a61234ffdf5449b6ef02455dd3
Simon Liu <cyamonide@gmail.com>
2019-07-21 21:08:07 -0400

    Remove frontend redirect

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/api", (req, res) => res.send("API endpoint"));
+app.get("/", (req, res) => res.send("API endpoint"));
 
-app.get("/:path(*)", (req, res) => res.redirect("http://localhost:3002/" + req.params.path));
+// app.get("/:path(*)", (req, res) => res.redirect("http://localhost:3002/" + req.params.path));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () =>
