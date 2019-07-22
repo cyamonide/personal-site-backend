51a32464804e2b358cd8ccddb61fce342ba0015d
Simon Liu <cyamonide@gmail.com>
2019-07-21 20:20:07 -0400

    Set up server-side routing

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => res.send("API endpoint"));
+
+app.get("/:path(*)", (req, res) => res.redirect("http://localhost:3002/" + req.params.path));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () =>
+  console.log(`Personal site backend listening on port ${port}`)
+);
