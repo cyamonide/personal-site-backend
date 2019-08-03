4ab6542ab5f5c0904cf7e8747564192c32ea5ebb
Simon Liu <cyamonide@gmail.com>
2019-08-03 05:44:35 -0400

    Add development environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,12 @@ module.exports = {
     script: 'node',
     interpreter: 'none',
     args: 'index.js',
+    env: {
+      NODE_ENV: 'development',
+      HOME_URL: 'http://localhost:3000',
+      API_URL: 'http://localhost:3001',
+      PORT: 3001
+    },
     env_production: {
       NODE_ENV: 'production',
       HOME_URL: 'http://cyamonide.me',
