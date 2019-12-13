41410a883e49e5778057aca7c6d78dc9719b3623
Simon Liu <cyamonide@gmail.com>
2019-12-12 21:23:58 -0500

    Add webhook handling for backend repo

--- index.js
+++ index.js
@@ -2,46 +2,55 @@ const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { exec } = require("child_process");
-const crypto = require('crypto');
-const safeCompare = require('safe-compare');
-const concat = require('concat-stream');
+const crypto = require("crypto");
+const safeCompare = require("safe-compare");
+const concat = require("concat-stream");
 
 const app = express();
 
-app.use(function(req, res, next){
-      req.pipe(concat(function(data){
-	      req.body = data;
-	      next();
-	    }));
+app.use(function(req, res, next) {
+  req.pipe(
+    concat(function(data) {
+      req.body = data;
+      next();
+    })
+  );
 });
 
+/* PDF resume redirect */
 app.get("/documents/resume", (req, res) => {
   res.redirect(`${process.env.API_URL}/documents/Resume%20-%20Simon%20Liu`);
 });
 
+/* PDF resume endpoint */
 app.get("/documents/Resume%20-%20Simon%20Liu", (req, res) => {
-    const path = __dirname + "/public/documents/resume.pdf";
-    fs.readFile(path, (err, data) => {
-      if (err) {
-        res.send(err.message);
-      }
-      else {
-        res.contentType("application/pdf");
-        res.send(data);
-      }
-    });
+  const path = __dirname + "/public/documents/resume.pdf";
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.contentType("application/pdf");
+      res.send(data);
+    }
   });
+});
 
+/* Webhooks endpoint */
 app.post("/webhooks/:repo", (req, res) => {
-  const computed = "sha1=" + 
-	crypto.createHmac('sha1', process.env.GIT_WEBHOOKS_SECRET)
-	      .update(req.body)
-	      .digest('hex');
+  // Verify secret
+  const computed =
+    "sha1=" +
+    crypto
+      .createHmac("sha1", process.env.GIT_WEBHOOKS_SECRET)
+      .update(req.body)
+      .digest("hex");
   const received = req.get("X-Hub-Signature");
   if (!safeCompare(computed, received)) {
     res.status(401).send("Incorrect secret.");
     return;
   }
+
+  // Update repository
   console.log(`Pulling ${req.params.repo}...`);
   exec(`git -C ~/${req.params.repo} pull`, (err, stdout, stderr) => {
     if (err) {
@@ -49,14 +58,20 @@ app.post("/webhooks/:repo", (req, res) => {
     } else {
       res.status(200).send(stdout);
     }
-    console.log(`Building ${req.params.repo}...`);
-    exec(`yarn --cwd ../${req.params.repo} build`, (err, stdout, stderr) => {
-      if (err) {
-        console.log(stderr);
-      } else {
-        console.log(stdout);
-      }
-    });
+    // Handle different repositories
+    if (req.params.repo == "personal-site-backend") {
+      // Restart pm2 server
+      console.log("Restarting pm2 server...");
+      exec("pm2 restart personal-site-backend", (err, stdout, stderr) => {
+        console.log(err ? stderr : stdout);
+      });
+    } else if (["personal-site-frontend", "resume"].indexOf(req.params.repo) > -1) {
+      // Build project
+      console.log(`Building ${req.params.repo}...`);
+      exec(`yarn --cwd ../${req.params.repo} build`, (err, stdout, stderr) => {
+        console.log(err ? stderr : stdout);
+      });
+    }
   });
 });
 
