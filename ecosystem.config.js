72375b7319dae7b8ebcf14c4dbcdc2c54efd9532
Simon Liu <cyamonide@gmail.com>
2019-12-21 21:55:11 -0500

    Update ecosystem env vars

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,26 @@
 module.exports = {
-  apps : [{
-    name: 'personal-site-backend',
-    script: 'node',
-    interpreter: 'none',
-    args: 'index.js',
-    env: {
-      NODE_ENV: 'development',
-      HOME_URL: 'http://localhost:3000',
-      API_URL: 'http://localhost:3001',
-      PORT: 3001
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      HOME_URL: 'http://cyamonide.me',
-      API_URL: 'http://api.cyamonide.me',
-      PORT: 3001
+  apps: [
+    {
+      name: "personal-site-backend",
+      script: "node",
+      interpreter: "none",
+      args: "index.js",
+      env: {
+        NODE_ENV: "development",
+        HOME_URL: "http://localhost:3000",
+        API_URL: "http://localhost:3001",
+        NOTES_URL: "http://localhost:3003",
+        RESUME_URL: "http://localhost:3004",
+        PORT: 3001
+      },
+      env_production: {
+        NODE_ENV: "production",
+        HOME_URL: "https://cyamonide.me/",
+        API_URL: "https://api.cyamonide.me/",
+        NOTES_URL: "https://notes.cyamonide.me/",
+        RESUME_URL: "https://resume.cyamonide.me/",
+        PORT: 3001
+      }
     }
-  }]
-}
+  ]
+};
